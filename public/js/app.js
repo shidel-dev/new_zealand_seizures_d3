0423fefd2f5d7985495e72c7470ae9ed35d887c1
Sam Blackman <joeshidel@gmail.com>
2014-02-23 14:31:36 -0600

    sorting of data and coloring

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
 d3.csv("nz_seizure_incidents_data.csv", function(totals) {
+
+  var uniqueGoods = extractUniqueGoods(totals)
+  console.log(uniqueGoods)
   var diameter = 960,
     format = d3.format(",d"),
     color = d3.scale.category20c();
@@ -27,7 +30,136 @@ d3.csv("nz_seizure_incidents_data.csv", function(totals) {
 
   node.append("circle")
     .attr("r", function(t) { return t.r })
-    .style("fill", function(d) { return "#ccccff"; });
+    .style("fill", function(d) {return deviceToColor(d["Description of Goods"])});
 
     d3.select(self.frameElement).style("height", diameter + "px");
+    combinePortInfo(totals)
 });
+
+function extractUniqueGoods(totals){
+  var set = {}
+  _.each(totals, function(item){
+    set[item["Description of Goods"]] = true;
+  });
+
+  return  _.keys(set)
+
+}
+
+function combinePortInfo(portIncidents) {
+  _.groupBy(portIncidents, function(item){
+    return deviceToCategory(item["Description of Goods"])
+  }))
+}
+
+
+function deviceToColor(device){
+  return colorsToCategories[deviceToCategory(device)]
+}
+
+function deviceToCategory(device){
+ return descriptionsToCategories[device]
+}
+
+
+var colorsToCategories = {"computer": "11A7FC",
+"cellphone": "95D127",
+"camera": "F2E415",
+"storage device": "FF8638",
+"media": "EE3551",
+"misc": "999999",
+"other": "CCCCCC"}
+
+var descriptionsToCategories = {
+  "Antenna - LAN cards": "other",
+  "Antenna - USB WiFi":"other",
+  "Antenna - WiFi": "other",
+  "ATM card slots": "other",
+  "ATM keypads": "other",
+  "Audio cassettes": "media",
+  "Bitcoin mining machine": "other",
+  "Blackberry": "cellphone",
+  "Cameras - video": "camera",
+  "Cameras - web": "camera",
+  "Cameras": "camera",
+  "Card readers": "other",
+  "CD/DVD reader - external": "other",
+  "Cellphone circuitboards": "other",
+  "Cellphone circuitry": "other",
+  "Cellphone parts": "other",
+  "Cellphones (iPhone)": "cellphone",
+  "Cellphones": "cellphone",
+  "Circuit board/chip": "other",
+  "Circuit boards": "other",
+  "Computer parts": "other",
+  "Computer": "computer",
+  "Computers - mini towers":  "computer",
+  "Computers - tablets": "computer",
+  "Computers": "computer",
+  "Discs - blu-ray": "media",
+  "Discs - CDs": "media",
+  "Discs - DVDs": "media",
+  "Discs - optical": "storage device",
+  "Discs": "storage device",
+  "DVD burners": "other",
+  "DVD players": "other",
+  "DVD recorders": "other",
+  "DVD writers": "other",
+  "E-book readers": "misc",
+  "E-reader": "misc",
+  "eReaders": "misc",
+  "Film": "media",
+  "Frequency/signal jammers": "other",
+  "GPS": "misc",
+  "Hard drives - external": "storage device",
+  "Hard drives - internal": "storage device",
+  "Hard drives": "storage device",
+  "Infrared detectors": "other",
+  "iPods": "computer",
+  "Laboratory equipment parts": "other",
+  "Laboratory evaporator unit": "other",
+  "Laptops": "computer",
+  "Laser pointer": "other",
+  "Laser pointers": "other",
+  "LCD modules": "other",
+  "Listening devices": "other",
+  "Media players": "misc",
+  "Modem stick": "other",
+  "Modem": "other",
+  "Modems": "other",
+  "Mouse USB drive": "other",
+  "MP3 players": "misc",
+  "Multi-media players": "misc",
+  "Net com device": "other",
+  "Parts": "other",
+  "Police scanners": "other",
+  "Portable storage devices": "storage device",
+  "Projectors": "other",
+  "Radiometer": "other",
+  "Receivers": "other",
+  "Remote access devices": "other",
+  "Remote controls": "other",
+  "RF signal detectors": "other",
+  "Routers": "other",
+  "Scales": "other",
+  "Scanners": "other",
+  "SIM card packs": "misc",
+  "SIM cards": "misc",
+  "SIM packs": "misc",
+  "Skimmer device": "other",
+  "Skimming devices": "other",
+  "Skimming equipment": "other",
+  "Software": "media",
+  "Speakerphone": "other",
+  "Swipe cards": "other",
+  "Tablet accessories": "other",
+  "Time and attendance machines": "other",
+  "USB connectors": "other",
+  "USB converter": "other",
+  "USB receiver/charger": "other",
+  "Video recorders": "other",
+  "Video tapes": "media",
+  "Voice recorders": "other",
+  "Walkmans": "misc",
+  "Wireless AU senders": "other"
+}
